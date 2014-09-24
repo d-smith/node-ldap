619597168e8d0439927b5be115165131bc47b577
d-smith <doug.smith.mail@gmail.com>
2014-09-24 08:29:15 -0600

    Added xtrac user to repo

--- simple.js
+++ simple.js
@@ -2,6 +2,14 @@ var ldap = require('ldapjs');
 
 var server = ldap.createServer();
 
+server.bind('cn=xtrac', function(req, res, next) {
+  if (req.dn.toString() !== 'cn=xtrac' || req.credentials !== 'secret')
+    return next(new ldap.InvalidCredentialsError());
+
+  res.end();
+  return next();
+});
+
 server.listen(1389, function() {
     console.log('LDAP server up at: %s', server.url);
 });
