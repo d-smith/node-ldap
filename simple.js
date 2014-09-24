61d284d63fa192d36ccb9fe75df299ff7b263a10
d-smith <doug.smith.mail@gmail.com>
2014-09-24 09:01:30 -0600

    Added simple search based on ldapjs sample

--- simple.js
+++ simple.js
@@ -10,6 +10,58 @@ server.bind('cn=xtrac', function(req, res, next) {
   return next();
 });
 
+function authorize(req, res, next) {
+  if (!req.connection.ldap.bindDN.equals('cn=xtrac'))
+    return next(new ldap.InsufficientAccessRightsError());
+
+  return next();
+}
+
+var foo = {
+  dn: 'cn=foo,ou=users,o=atlas',
+  attributes: {
+    cn: 'foo',
+    uid: 1,
+    gid: 'wheel',
+    description: 'a user',
+    objectClass: 'xtracUser'
+  }
+};
+
+var bar = {
+  dn: 'cn=bar,ou=users,o=atlas',
+  attributes: {
+    cn: 'bar',
+    uid: 2,
+    gid: 'wheel',
+    description: 'another user',
+    objectClass: 'xtracUser'
+  }
+};
+
+
+var users = {};
+users['foo'] = foo;
+users['bar'] = bar;
+
+function loadUsers(req, res, next) {
+  req.users = users;
+  return next();
+}
+
+var pre = [authorize, loadUsers];
+
+server.search('o=atlas', pre, function(req, res, next) {
+
+  Object.keys(req.users).forEach(function(k) {
+    if (req.filter.matches(req.users[k].attributes))
+      res.send(req.users[k]);
+  });
+
+  res.end();
+  return next();
+});
+
 server.listen(1389, function() {
     console.log('LDAP server up at: %s', server.url);
 });
